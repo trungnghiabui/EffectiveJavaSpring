444935c99ee6274461397df1795155708a7a7b3f
nghiabt <nghiabt@admins-MacBook-Air.local>
2023-09-24 00:13:31 +0700

    First commit

---
+++ src/main/java/com/example/effectivejavawithspring/EffectiveJavaWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.effectivejavawithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffectiveJavaWithSpringApplication.class, args);
+	}
+
+}
